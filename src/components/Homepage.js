98bd8bd63370e63e1d201dcb145a84e09f6c18d5
RajeshRao93 <rajesh.rao0593@gmail.com>
2022-05-01 13:28:36 +0200

    adding href to anchor tag - 2

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -38,7 +38,7 @@ const Homepage = () => {
         <AboutMe />
       </div>
       <div className="copyright">
-        <a href="" target="_blank">
+        <a href="https://www.hitwebcounter.com" target="_blank">
           Hits:
           <img
             src="https://hitwebcounter.com/counter/counter.php?page=7982168&style=0006&nbdigits=5&type=page&initCount=0"
