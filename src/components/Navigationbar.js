445ef0b5b79203f12b470378fa5ea9e1b3bca65f
RajeshRao93 <rajesh.rao0593@gmail.com>
2024-02-27 17:34:09 +0100

    remove unused vars

--- src/components/Navigationbar.js
+++ src/components/Navigationbar.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Navbar } from "react-bootstrap";
 import { MDBIcon } from "mdbreact";
 import "./Navigationbar.css";
